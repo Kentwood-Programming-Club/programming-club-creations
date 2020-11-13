27f69b4a94009b8b32f8183f9e1468a033c2b0e9
Nathaniel D'Rozario <nsdrozario@gmail.com>
2020-11-13 12:58:02 -0800

    canvas drawing library + demonstration of canvas drawing

---
+++ canvas-drawing/easyhtmlcanvas.js
@@ -0,0 +1,26 @@
+const EasyHTMLCanvas = {
+    createCanvas: function(id) {
+       const canvas = document.createElement("canvas");
+       canvas.id = id;
+       
+       // default values
+       canvas.width = 800;
+       canvas.height = 600;
+
+       document.body.appendChild(canvas);
+       return canvas.getContext("2d");
+    },
+    drawSquare: function(ctx, posX, posY, sizeX, sizeY, color) {
+        ctx.fillStyle = color;
+        ctx.fillRect(posX, posY, sizeX, sizeY);
+    },
+    drawCircle: function(ctx, posX, posY, radius) {
+        ctx.fillStyle = color;
+        ctx.fillCircle(posX, posY, radius);
+    },
+    clear: function(ctx) {
+        ctx.clearRect(0,0, ctx.canvas.clientWidth,ctx.canvas.clientHeight);
+    },
+    genColorHSL: (h,s,l) => "hsl(" + h + ", " + s + "%, " + l + "%)",
+    genColorRGB: (r,g,b) => "rgb(" + r + ", " + g + ", " + b + ")",
+}
