ab4947ae116802c750399ee3deb1030d00356fb6
Baye <73002864+Grayhound2@users.noreply.github.com>
2021-02-02 17:30:38 -0800

    Update trial101.js

--- simple-calculator/trial101.js
+++ simple-calculator/trial101.js
@@ -1,3 +1,16 @@
-<script>
-    
-</script>
+function add(a,b){
+    let value = a+b;
+    return value 
+}
+function subtract(a,b){
+    let value = a-b;
+    return value
+}
+function multiply(a,b){
+    let value = a*b;
+    return value;
+}
+function divide(a,b){
+    let value = a/b;
+    return value;
+}
