0f9bea5e35ae512ce8578d393fd52bc91bb31def
Petaboi <ptr.dodge@gmail.com>
2021-04-30 16:42:43 -0700

    styled

--- planner/api.js
+++ planner/api.js
@@ -1,12 +1,14 @@
 function add_event(element) {
     element.innerHTML +=
-    `
-    <div class='event'>
-        <input type='text' placeholder='Event info' />
-        <p>Duration: <input type='text' /></p>
-        <button onclick='this.parentElement.remove()'>Delete</button>
-    </div>
-    `;
+        `<div class='event'>
+            <input type='text' placeholder='Event info' />
+            <p>Duration: <input type='text' /></p>
+            <button onclick='removeEvent(this)'>Delete</button>
+        </div>`;
+}
+
+const removeEvent = (event) => {
+    event.parentElement.remove();
 }
 
 function populate_hours() {
@@ -15,7 +17,7 @@ function populate_hours() {
     for (let i = 0; i < blocks.length; i++) {
         blocks[i].innerHTML = `<td class='num'>${get_hour_from_num(hours)}</td>`;
         for (let j = 0; j < 7; j++) {
-            blocks[i].innerHTML += "<td><button onclick='add_event(this.parentElement)'>New Event</button></td>";
+            blocks[i].innerHTML += "<td><button class='new-event' onclick='add_event(this.parentElement)'>New Event</button></td>";
         }
         hours++;
     }
--- planner/date_util.js
+++ planner/date_util.js
@@ -1,3 +1,4 @@
 function get_hour_from_num(num) {
     // Implement this
+    return "this still needs some work";
 }
