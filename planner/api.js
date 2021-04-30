ae7161606dd0a2dadb059d66b2ede9ff90a661a0
Nishchal Shukla <sn362438@kent.k12.wa.us>
2021-04-30 14:49:30 -0700

    hehe design

--- planner/api.js
+++ planner/api.js
@@ -2,8 +2,8 @@ function add_event(element) {
     element.innerHTML +=
     `
     <div class='event'>
-        <p contenteditable='true'>Event info</p>
-        <p>Duration: <span contenteditable='true'>[duration]</span></p>
+        <input type='text' placeholder='Event info' />
+        <p>Duration: <input type='text' /></p>
         <button onclick='this.parentElement.remove()'>Delete</button>
     </div>
     `;
@@ -13,7 +13,7 @@ function populate_hours() {
     let blocks = document.getElementsByClassName("hour-block");
     let hours = 0;
     for (let i = 0; i < blocks.length; i++) {
-        blocks[i].innerHTML = `<td>${get_hour_from_num(hours)}</td>`;
+        blocks[i].innerHTML = `<td class='num'>${get_hour_from_num(hours)}</td>`;
         for (let j = 0; j < 7; j++) {
             blocks[i].innerHTML += "<td><button onclick='add_event(this.parentElement)'>New Event</button></td>";
         }
