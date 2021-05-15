71b7f0cf93ee151e4a7a3ee892935afb72a9a117
Jooyoung1106 <airplane961@gmail.com>
2021-05-14 18:47:10 -0700

    Update date_util.js

--- planner/date_util.js
+++ planner/date_util.js
@@ -1,29 +1,24 @@
 function get_hour_from_num(num) {
-  var hourNum = num + 1; // add 1 to make it display hour 1-12 instead of 0-11
+  var hourNum = num; // deleted '+ 1' and make 0 to 12 AM
   var text = hourNum; //store the new number in another variable
   if (hourNum < 12) {
     text = hourNum + "&nbsp;AM";
-    /* im using '&nbsp;' instead of ' ' becasue
-     i hoestly dont like the way a hanging 
-     space looks at the beginning or end 
-     of a string, but its up to you really.*/
   }
-/* i know i know im not using an else, 
-  but this makes it a little easier to read
-  and also makes it so that 'undefined'
-  or 'null' cannot show up on screen */
+
   if (hourNum == 12){
     text = hourNum + "&nbsp;PM";
   }
+
   if (hourNum > 12) {
-    // check if our number is greater than or equal to 12
-    // if so, we put some text onto the end of it so send it to the return statement.
     text = hourNum - 12 + "&nbsp;PM";
-    /* we are subtracting 11 because '0' 
-    is a number too, so 11 is the 12th number*/
+  }
+
+  if (hourNum == 0) { // if hourNum is 0, 
+    text = hourNum + 12 + "&nbsp;AM"; // add 12 with AM so that comes with 12 AM. I don't know if I have to do some return 0 thing. Anyway, it works. 
   }
   return text;
 }
 
-/* still needs some work to omit the '13 PM' 
-at the bottom, but fonctionslly works and makes sense now */
+// 13 PM issue resolved
+// add(changed) 12 AM so the day start with 12 AM, not 1 AM.
+// I erased all //'s so it seem better. (Of ocurse I understand that all) I wrote some :)
